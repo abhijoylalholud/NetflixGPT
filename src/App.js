eff35d32f4dae3ea62e2dc433e8147a68bdbb2d0
Abhijoy Samaddar <samaddar.abhijoy@gmail.com>
2025-02-20 18:45:45 +0530

    Tested git, working successfully

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="font-bold text-3xl text-green-800">Namaste Everyone!!</div>
+		<div className="font-bold text-3xl text-green-800">Namaste Everyone!! Joy East Bengal</div>
 	);
 }
 
